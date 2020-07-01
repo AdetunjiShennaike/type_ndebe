4e18cd3db548ae65776da90a272a784f01eaadde
Adetunji <Ashennaike@gmail.com>
2020-07-01 14:01:46 -0400

    styling mostly if not all done and adding fucntionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [text, setText] = useState()
+
+  const inputHandler = e => {
+    setText(e.input)
+  }
+
+  const shareCurrent = () => {
+    
+  }
+
+
+
   return (
     <div className="App">
       <header>
         <img src='./ndebeblackicon.png' alt='logo' />
+        <p>Social Media Link</p>
       </header>
       <div className='container'>
         <div className='leftside'>
             <h2>Type Ndebe</h2>
             <h3>This changes your text from english to ndebe like so:</h3>
-            <p>man <span>to</span> <img src='./igboscript' alt='something'/></p>
-            <p>woman <span>to</span> <img src='./igboscript' alt='something'/></p>
-            <p>child <span>to</span> <img src='./igboscript' alt='something'/></p>
+            <p>man <span>to</span> <img src='./igboscript.png' alt='something'/></p>
+            <p>woman <span>to</span> <img src='./igboscript.png' alt='something'/></p>
+            <p>child <span>to</span> <img src='./igboscript.png' alt='something'/></p>
         </div>
         <div className='rightside'>
           <div className='top'>
             <h2> Type a message here</h2>
             <label>Maximum character count is 128</label>
-            <input name='translate' type='text' />
-            <button onClick >Share Your Ndebe Text</button>
+            <input onChange={inputHandler} name='translate' type='text' maxLength={128} />
+            <button onClick={shareCurrent} >Share Your Ndebe Text</button>
           </div>
           <div className='bottom'>
-            <img src='./logo.svg' alt='typed' />
+            <img src='./ndebeblackicon.png' alt='typed' />
           </div>
         </div>
       </div>
